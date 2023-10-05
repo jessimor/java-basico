9ab6c194b289d2fb448172680085e78dadf4140d
Jessica Moreira <jessicamoreira51858@gmail.com>
2023-10-05 19:12:22 -0300

    Aulas-JAVA

---
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -0,0 +1,32 @@
+public class MinhaClasse {
+
+    public static void main (String[] args) {
+        System.out.print ("Boa sorte!!!");
+    }
+}
+
+//Os oito tipos primitivos em Java são: int, byte, short, long, float, double, boolean e char
+
+//tipo float tem que colocar um F no final. Ex: float pi= 3.14F;
+//o int não pode começar com 0. se for um CEP iniado com 0 por ex teria que mudar para tipo string
+// o tipo long precisa ser encerrado com um L. Ex: long cpf = 23115889886L;
+
+//para deixar uma variavel constante é preciso colocar a palavra 'final' e nomea-la com letras maiusculas
+//Ex: final double VALOR_DE_PI = 3.14;
+
+//para atribuir valor para string usamos aspas nome = "Jessica";
+//para número não precisa: int idade = 22;
+// no caso de caracter usar aspas simples: char sexo = 'M'; (e só permite um único caracter)
+
+//criando um novo objeto na nossa aplicação: Date dataNascimento = new Date();
+
+//incrementar ou decrementar valores de 1 em 1 ++ e --;
+// ! inverte o valor da variavel booleana. ex: boolean variavel = true; System.out.print(!variavel);
+
+//operador ternário é como se fosse um if expressado em uma linha só.
+//Ex.: int a,b; a = 5; b=6;
+//String resultado = a==b ?"verdadeiro" : "falso";
+
+//metodo equals compara conteúdo (compara se o conteúdo de dois objetos são iguais)
+// para número utilizamos operadores realacionais == != > < >= <=
+//Operadores lógicos && 'E' || 'OU'
